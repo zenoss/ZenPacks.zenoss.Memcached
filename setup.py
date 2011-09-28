3f0379c82c3c24de0f4794cad6fa5e7abb0d5d4d
Chet Luther <chet.luther@gmail.com>
2011-09-28 09:01:30 -0400

    Update author. PEP8 cleanup of setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # NB: PACKAGES is deprecated
 NAME = 'ZenPacks.zenoss.Memcached'
 VERSION = '0.8.0'
-AUTHOR = 'Zenoss'
+AUTHOR = 'Zenoss Labs'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.Memcached']
@@ -21,38 +21,38 @@ setup(
     # This ZenPack metadata should usually be edited with the Zenoss
     # ZenPack edit page.  Whenever the edit page is submitted it will
     # overwrite the values below (the ones it knows about) with new values.
-    name = NAME,
-    version = VERSION,
-    author = AUTHOR,
-    license = LICENSE,
-    
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    license=LICENSE,
+
     # This is the version spec which indicates what versions of Zenoss
     # this ZenPack is compatible with
-    compatZenossVers = COMPAT_ZENOSS_VERS,
-    
+    compatZenossVers=COMPAT_ZENOSS_VERS,
+
     # previousZenPackName is a facility for telling Zenoss that the name
     # of this ZenPack has changed.  If no ZenPack with the current name is
     # installed then a zenpack of this name if installed will be upgraded.
-    prevZenPackName = PREV_ZENPACK_NAME, 
-    
+    prevZenPackName=PREV_ZENPACK_NAME,
+
     # Indicate to setuptools which namespace packages the zenpack
     # participates in
-    namespace_packages = NAMESPACE_PACKAGES,
-    
+    namespace_packages=NAMESPACE_PACKAGES,
+
     # Tell setuptools what packages this zenpack provides.
-    packages = find_packages(),
-    
+    packages=find_packages(),
+
     # Tell setuptools to figure out for itself which files to include
     # in the binary egg when it is built.
-    include_package_data = True,
-    
+    include_package_data=True,
+
     # Tell setuptools what non-python files should also be included
     # with the binary egg.
-    package_data = { 
+    package_data={
          '': ['*.txt'],
-         NAME: ['objects/*','skins/*/*','services/*', 'reports/*/*',
+         NAME: ['objects/*', 'skins/*/*', 'services/*', 'reports/*/*',
                 'modeler/*/*', 'daemons/*', 'lib/*', 'libexec/*'],
-         },  
+         },
 
     # Indicate dependencies on other python modules or ZenPacks.  This line
     # is modified by zenoss when the ZenPack edit page is submitted.  Zenoss
@@ -60,14 +60,14 @@ setup(
     # list, so any manual additions should be added to the end.  Things will
     # go poorly if this line is broken into multiple lines or modified to
     # dramatically.
-    install_requires = INSTALL_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
 
     # Every ZenPack egg must define exactly one zenoss.zenpacks entry point
     # of this form.
-    entry_points = {
+    entry_points={
         'zenoss.zenpacks': '%s = %s' % (NAME, NAME),
     },
 
     # All ZenPack eggs must be installed in unzipped form.
-    zip_safe = False,    
+    zip_safe=False,
 )
